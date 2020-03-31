48c6459368a828c0b6fb38e831e7bfcfab0d4dd5
fanga2 <fanga2>
2020-03-31 13:30:38 -0400

    Added onAuthorized function, auth headers and inital GET request

--- MinimumOverlay/viewer.js
+++ MinimumOverlay/viewer.js
@@ -1,13 +1,33 @@
 const directions = ['#uparrow', '#downarrow', '#leftarrow', '#rightarrow', '#stop', '#reverse']
+let token = ''
+let uID = ''
+let cID = ''
+let userName = ''
+let channelName = ''
 
 function move_request(data){
     return {
         type: 'POST',
         url: location.protocol + '//localhost:5000/send_move',
         dataType: 'text',
-        data: data
+        data: data,
+        headers: { 'Authorization': 'Bearer ' + token}
     };
 }
+
+twitch.onAuthorized(function (auth){
+    token = auth.token
+    uID = auth.userID
+    cID = auth.channelID
+
+    $.ajax({
+        url: location.protocol + '//localhost:5000/send_move',
+        type: 'GET',
+        headers: {
+          'x-extension-jwt': auth.token,
+        }
+    });
+});
 
 $(function (){
     for(const property in directions){
