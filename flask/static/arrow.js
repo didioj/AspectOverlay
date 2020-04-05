b32cea03d3a4a3fe325b0f4daa6280be6054bee3
Jakob Didio <didioj2@rpi.edu>
2020-04-05 15:31:25 -0700

    Added visual queue of actions.

--- flask/static/arrow.js
+++ flask/static/arrow.js
@@ -1,3 +1,6 @@
+const max_moves = 5;
+var current_moves = 0;
+
 async function move_request(dir) {
     console.log(dir)
     res = await fetch(location.protocol + '//localhost:5000/api/send_move', {
@@ -11,5 +14,32 @@ async function move_request(dir) {
     });
     if (!res.ok) {
         throw new Error('Sending move_request: ' + res.statusText);
+    }
+}
+
+function add_arrow(dir)
+{
+    console.log(dir)
+    if (current_moves === max_moves)
+    {
+        let queue = document.getElementById('actionqueue');
+        console.log("Maximum moves reached, deleting...");
+        while (queue.children.length !== 0)
+        {
+            queue.removeChild(queue.children[0]);
+        }
+        current_moves = 0;
+    }
+    else
+    {
+        console.log("Adding " + dir + " arrow");
+        holder = document.getElementById('actionqueue');
+        newChild = document.createElement('img');
+        newChild.setAttribute('src', '../static/' + dir + '-arrow.svg');
+        newChild.setAttribute('id', dir + 'arrow');
+        newChild.setAttribute('width', 100);
+        newChild.setAttribute('height', 100);
+        holder.appendChild(newChild);
+        current_moves++;
     }
 }
