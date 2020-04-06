1630f840d7d25cfb64fc4618c06b21649aadda4e
Wilson Wong <wilsonwong1688@gmail.com>
2020-04-06 00:41:46 -0400

    Finished submit button

--- flask/static/arrow.js
+++ flask/static/arrow.js
@@ -33,24 +33,43 @@ function render_queue() {
 
 function add_arrow(dir) {
     if (current_moves.length === max_moves) {
-        current_moves = [];
+        //current_moves = [];
+        return;
     }
     current_moves.push(dir);
     render_queue();
+    /*
     if (current_moves.length === max_moves) {
         move_request(current_moves);
     }
+    */
 }
 
 function undo()
 {
-    if(current_moves === 0)
+    if(current_moves.length === 0)
     {
         console.log("No actions to remove");
         return;
     }
-    console.log("Removing last action");
+    //console.log("Removing last action");
     holder = document.getElementById('actionqueue');
-    holder.removeChild(holder.childNodes[current_moves-1]);
-    current_moves--;
+    holder.removeChild(holder.childNodes[current_moves.length-1]);
+    action = current_moves.pop();
+    console.log("Action " + action + " removed successfully");
+}
+
+function submit(){
+    if (current_moves.length === max_moves) {
+        console.log("Submitting queue of moves:");
+        console.log(current_moves);
+        move_request(current_moves);
+        current_moves = [];
+        while (holder.hasChildNodes()) {
+            holder.removeChild(holder.lastChild);
+        }
+    }
+    else{
+        console.log("Not enough actions queued");
+    }
 }
