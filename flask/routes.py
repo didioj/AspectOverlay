5fa18fdacaabc0b23dd49c54d2377367ba3dd22f
Maya Tung <mktung0@gmail.com>
2020-04-06 15:43:18 -0400

    backend sanitization and animations

--- flask/routes.py
+++ flask/routes.py
@@ -11,15 +11,20 @@ def index():
 def send_move():
    if not request.json or not 'moves' in request.json:
       abort(400)
+   moves = request.json['moves']
+   if (len(moves) != 5):
+      return {'error': 'The list of moves must be 5 long'}
+   filter(lambda x: x in ['left', 'down', 'stand', 'up', 'right'], moves)
+   if (len(moves) != 5):
+      return {'error': 'The list of moves must only contain "left", "down", "stand", "up", or "right"'}
    res = requests.post(
             'http://127.0.0.1:4000/',
             data = {'moves':' '.join(request.json['moves'])},
             headers = {'content-type':'application/json'}
          )
    if (res.status_code != requests.codes.ok):
+      return {'error': 'The list of moves must only contain "left", "down", "stand", "up", or "right"'}
       print('There was an error sending moves to the game')
-   else:
-      print('Sent moves to the game')
    return {}
 
 if __name__ == '__main__':
